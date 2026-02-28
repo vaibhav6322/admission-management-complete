125bafc2a2e7e36eaa35250573af4cc2e4f44c8e
vaibhav tripathi <vaibhavtripathi272001lko@gmail.com>
2026-02-28 15:49:06 +0530

    update

--- server/src/server.js
+++ server/src/server.js
@@ -1,3 +1,9 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./config/db");
+
+connectDB();
 const app = express();
 
 const allowedOrigins = [
@@ -19,3 +25,13 @@ app.use(
 );
 
 app.use(express.json());
+
+app.use("/api/auth", require("./routes/auth.routes"));
+app.use("/api/dashboard", require("./routes/dashboard.routes"));
+app.use("/api/master", require("./routes/master.routes"));
+app.use("/api/applicant", require("./routes/applicant.routes"));
+app.use("/api/admission", require("./routes/admission.routes"));
+
+app.listen(process.env.PORT || 4000, () =>
+  console.log("Server Running on port", process.env.PORT || 4000)
+);
